287b4f9f0e7b6bf9bb17f82451c1abd58977908c
Erik <ermorris@csumb.edu>
2024-07-16 17:45:54 -0700

    Initial Commit

---
+++ CSDL/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  CSDL
+//
+//  Created by Erik Morris on 7/16/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ CSDL/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  CSDL
+//
+//  Created by Erik Morris on 7/16/24.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
