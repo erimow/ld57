18cd51b7fba4609d69fd6b315f7d3e1773dbc4c6
erimow <erik.morris922@gmail.com>
2024-11-21 18:03:06 -0800

    Added some comments and tested some linux side stuff

--- Template/Engine/constants.h
+++ Template/Engine/constants.h
@@ -11,7 +11,9 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 #define TARGET_FPS 60
-#define TILE_SIZE 32;
-#define TILES_PER_GRID 36;
+#define TILE_SIZE                                                              \
+  32; // How many pixels for each tile sprite in the spriteSheet ex. 32 for a
+      // 32x32 sprite
+#define TILES_PER_GRID 36; // How many tiles Per grid TOTAL. Ex. 4x4 grid = 16
 
 #endif /* constants_h */
