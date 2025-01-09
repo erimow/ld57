cfe631af486c57cffb022c899722cf4aa151dd54
Erik Morris <erikmorris@poperk.san.rr.com>
2025-01-09 11:39:51 -0800

    Added some scene functionality but more needs to be done.

--- Fudgie/Scripts/game.c
+++ Fudgie/Scripts/game.c
@@ -1,6 +1,7 @@
 #include "../Engine/constants.h"
 #include "../Engine/context.h"
 #include "../Engine/efuncs.h"
+#include "./maingamescene.c"
 #include "card.h"
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_rect.h>
@@ -10,29 +11,15 @@
 
 typedef enum Scenes { MAINMENU, GAME } Scenes;
 static Scenes currentScene;
-static SDL_FRect handLocation = {
-    (float)SCREEN_WIDTH / 4 - ((float)SCREEN_WIDTH / 4) / 2,
-    SCREEN_HEIGHT - (float)SCREEN_HEIGHT / 4,
-    SCREEN_WIDTH - (float)SCREEN_WIDTH / 4, (float)SCREEN_HEIGHT / 4};
-static SDL_FPoint mousePos;
-static Card cardsInHand[7];
-static Card c;
 
 static void loadAssets(context *ctx) {
-  if (!Texture_loadFromFile(&c.CardBackdrop, ctx->renderer,
-                            "Art/CardBackdrop.png"))
-    printf("Could not load CardBackdrop\n");
+  maingamescene_loadAssets(ctx->renderer);
 }
 
 static void Game_Start(context *ctx) {
   loadAssets(ctx);
   currentScene = GAME;
-  c.isSelected = false;
-  c.pos = (SDL_FRect){50, 100, 100, 150};
-  for (int i = 0; i < 7; i++) {
-    cardsInHand[i] = c;
-    cardsInHand[i].pos.x += 150 * i;
-  }
+  maingamescene_start();
 }
 
 static void Game_Update(context *ctx) {
@@ -40,62 +27,32 @@ static void Game_Update(context *ctx) {
   case MAINMENU:
     break;
   case GAME:
-    // call games function
+    maingamescene_update();
     break;
-  }
-  for (int i = 0; i < 7; i++) {
-    Card *a = &cardsInHand[i];
-    if (a->isSelected) {
-      a->pos.x = mousePos.x - cardsInHand[i].whenSelectedMousePos.x;
-      a->pos.y = mousePos.y - cardsInHand[i].whenSelectedMousePos.y;
-    }
   }
 }
 
 static void Game_Render(context *ctx) {
-  SDL_SetRenderDrawColor(ctx->renderer, 255, 100, 0, 255);
-  SDL_RenderDrawRectF(ctx->renderer, &handLocation);
-  for (int i = 0; i < 7; i++) {
-    Card_Render(&cardsInHand[i], ctx->renderer);
-  }
   switch (currentScene) {
   case MAINMENU:
     break;
   case GAME:
-    // call games function
+    maingamescene_render(ctx->renderer);
     break;
   }
 }
 
 static void Game_Stop(context *ctx) {
-  printf("stopping game\n");
-  Texture_free(&c.CardBackdrop);
-  for (int i = 0; i < 7; i++) {
-    Texture_free(&cardsInHand[i].CardBackdrop);
-  }
-  printf("game assets stopped\n");
+  printf("Stopping game\n");
+  maingamescene_stop();
 }
-static void getMousePos(SDL_Event *e);
 static void Game_Events(context *ctx, SDL_Event *e) {
-  getMousePos(e);
-  for (int i = 0; i < 7; i++) {
-    Card_HandleEvents(&cardsInHand[i], e, mousePos);
-  }
+
   switch (currentScene) {
   case MAINMENU:
     break;
   case GAME:
-    // call games function
+    maingamescene_events(e);
     break;
-  }
-}
-
-static void getMousePos(SDL_Event *e) {
-  if (e->type == SDL_MOUSEMOTION) {
-    // Get mouse position
-    int x, y;
-    SDL_GetMouseState(&x, &y);
-    mousePos.x = x;
-    mousePos.y = y;
   }
 }
---
+++ Fudgie/Scripts/maingamescene.c
@@ -0,0 +1,70 @@
+#include "../Engine/constants.h"
+#include "../Engine/context.h"
+#include "../Engine/efuncs.h"
+#include "card.h"
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_render.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+static SDL_FRect handLocation = {
+    (float)SCREEN_WIDTH / 4 - ((float)SCREEN_WIDTH / 4) / 2,
+    SCREEN_HEIGHT - (float)SCREEN_HEIGHT / 4,
+    SCREEN_WIDTH - (float)SCREEN_WIDTH / 4, (float)SCREEN_HEIGHT / 4};
+static SDL_FPoint mousePos;
+static Card cardsInHand[7];
+static Card c;
+
+static void maingamescene_loadAssets(SDL_Renderer *renderer) {
+  if (!Texture_loadFromFile(&c.CardBackdrop, renderer, "Art/CardBackdrop.png"))
+    printf("Could not load CardBackdrop\n");
+}
+
+static void maingamescene_start() {
+  c.isSelected = false;
+  c.pos = (SDL_FRect){50, 100, 100, 150};
+  for (int i = 0; i < 7; i++) {
+    cardsInHand[i] = c;
+    cardsInHand[i].pos.x += 150 * i;
+  }
+}
+static void maingamescene_update() {
+  for (int i = 0; i < 7; i++) {
+    Card *a = &cardsInHand[i];
+    if (a->isSelected) {
+      a->pos.x = mousePos.x - cardsInHand[i].whenSelectedMousePos.x;
+      a->pos.y = mousePos.y - cardsInHand[i].whenSelectedMousePos.y;
+    }
+  }
+}
+static void maingamescene_render(SDL_Renderer *renderer) {
+  SDL_SetRenderDrawColor(renderer, 255, 100, 0, 255);
+  SDL_RenderDrawRectF(renderer, &handLocation);
+  for (int i = 0; i < 7; i++) {
+    Card_Render(&cardsInHand[i], renderer);
+  }
+}
+
+static void maingamescene_stop() {
+  Texture_free(&c.CardBackdrop);
+  for (int i = 0; i < 7; i++) {
+    Texture_free(&cardsInHand[i].CardBackdrop);
+  }
+}
+static void getMousePos(SDL_Event *e);
+static void maingamescene_events(SDL_Event *e) {
+  getMousePos(e);
+  for (int i = 0; i < 7; i++) {
+    Card_HandleEvents(&cardsInHand[i], e, mousePos);
+  }
+}
+static void getMousePos(SDL_Event *e) {
+  if (e->type == SDL_MOUSEMOTION) {
+    // Get mouse position
+    int x, y;
+    SDL_GetMouseState(&x, &y);
+    mousePos.x = x;
+    mousePos.y = y;
+  }
+}
