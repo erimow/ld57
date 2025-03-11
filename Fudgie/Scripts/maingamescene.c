06352b912041ca459cbed516f8eb5671431a31bb
Erk <erik.morris922@gmail.com>
2025-03-11 00:40:01 -0700

    Beginning actual game implementation

--- Fudgie/Scripts/maingamescene.c
+++ Fudgie/Scripts/maingamescene.c
@@ -4,6 +4,8 @@
 #include "card.h"
 #include "deck.h"
 
+#include <SDL3/SDL_rect.h>
+#include <SDL3/SDL_stdinc.h>
 #include <SDL3_ttf/SDL_ttf.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -23,13 +25,16 @@ static SDL_FPoint mousePos;
 static Card *cardBeingHeld = NULL;
 static Card *cardSelected = NULL;
 static unsigned int round = 8;
+static Uint8 playerCount;
 static Phase currentPhase;
 static Deck deck;
 static Player *players = NULL;
+static Player *playerPlaying = NULL;
+static Texture *playerText = NULL;
 static Button playbutt; // test
 static Button predictionButtons[9];
 static Texture roundText;
-static Texture currentPredictionText;
+// static Texture currentPredictionText;
 static Texture phaseOrTurnText;
 
 static void maingamescene_loadAssets(
@@ -43,7 +48,7 @@ static void maingamescene_loadAssets(
                      ((float)SCREEN_WIDTH / 2) - 15, 120, 50,
                      "Art/ButtonBackground.png", ctx->gFont, "Play", 4,
                      (SDL_Color){5, 5, 5, 255});
-  for (int i = 0; i < 9; i++) {
+  for (int i = 0; i < round+1; i++) {
     char text[2];
     snprintf(text, 2, "%d", i);
     Button_initAndLoad(&predictionButtons[i], ctx->renderer,
@@ -59,9 +64,9 @@ static void maingamescene_loadAssets(
   Texture_init_andLoadFromRenderedText(
       &roundText, ctx->renderer, ctx->gFont, (SDL_FRect){15, 15, 250, 60},
       "Round of 8", 10, (SDL_Color){255, 255, 255, 255});
-  Texture_init_andLoadFromRenderedText(
-      &currentPredictionText, ctx->renderer, ctx->gFont, (SDL_FRect){15, 75, 200, 30},
-      "Current prediction: -1", 22, (SDL_Color){255, 255, 255, 255});
+  // Texture_init_andLoadFromRenderedText(
+  //     &currentPredictionText, ctx->renderer, ctx->gFont, (SDL_FRect){15, 75, 200, 30},
+  //     "Current prediction: -1", 22, (SDL_Color){255, 255, 255, 255});
   Texture_init_andLoadFromRenderedText(
       &phaseOrTurnText, ctx->renderer, ctx->gFont, (SDL_FRect){((float)SCREEN_WIDTH/2)-125, 15, 250, 60},
       "Prediction Phase", 16, (SDL_Color){255, 255, 255, 255});
@@ -70,6 +75,7 @@ static void maingamescene_loadAssets(
 static void
 maingamescene_start(void *ct) { //---------------------------------------------------START
   context *ctx = (context*)ct;
+  playerCount = ctx->numPlayas;
 
   Deck_init(&deck);
   Deck_scramble(&deck);
@@ -78,6 +84,12 @@ maingamescene_start(void *ct) { //----------------------------------------------
   }
   players = (Player *)malloc(sizeof(Player) * ctx->numPlayas);
   Player_InitPlayers(players, ctx->numPlayas);
+  playerText = (Texture *)malloc(sizeof(Texture)* ctx->numPlayas);
+  for (int i = 0; i<ctx->numPlayas; i++){
+    char t[50];
+    snprintf(t, 50, "Player %d: -1",i+1);
+    Texture_init_andLoadFromRenderedText(&playerText[i], ctx->renderer, ctx->gFont, (SDL_FRect){15,75+(i*30),200,30}, t, 12, (SDL_Color){255,255,255,255});
+  }
   Deck_deal(&deck, players, ctx->numPlayas, round);
   currentPhase = predict;
 }
@@ -90,15 +102,15 @@ static void maingamescene_update(
     currentPhase = play;
     break;
   case predict:
-  for (int i = 0; i<9;i++){
+  for (int i = 0; i<round+1;i++){
     if (predictionButtons[i].isButtPressed){
       players[0].currentPrediction=i;
       predictionButtons[i].isButtPressed=false;
       char t[22];
-      snprintf(t, 22, "Current Prediction: %d", i);
+      snprintf(t, 22, "Player 1: %d", i);
   Texture_init_andLoadFromRenderedText(
-      &currentPredictionText, ctx->renderer, ctx->gFont, (SDL_FRect){15, 75, 200, 30},
-      t, 21, (SDL_Color){255, 255, 255, 255});
+      &playerText[0], ctx->renderer, ctx->gFont, (SDL_FRect){15, 75, 200, 30},
+      t, 11, (SDL_Color){255, 255, 255, 255});
       currentPhase=play;
   Texture_init_andLoadFromRenderedText(
       &phaseOrTurnText, ctx->renderer, ctx->gFont, (SDL_FRect){((float)SCREEN_WIDTH/2)-125, 15, 250, 60},
@@ -153,8 +165,11 @@ static void maingamescene_render(
 
     Texture_render(&phaseOrTurnText, renderer, NULL, NULL, 0.0, NULL, SDL_FLIP_NONE);
     Texture_render(&roundText, renderer, NULL, NULL, 0.0, NULL, SDL_FLIP_NONE);
-     for (int i = 0; i < 9; i++) {
+     for (int i = 0; i < round+1; i++) {
       Button_render(&predictionButtons[i], renderer);
+    }
+    for (int i = 0; i<playerCount; i++){
+      Texture_render(&playerText[i], renderer, NULL, NULL, 0.0, NULL, SDL_FLIP_NONE);
     }
     break;
   case play:
@@ -166,8 +181,11 @@ static void maingamescene_render(
       Button_render(&playbutt, renderer);
 
     Texture_render(&phaseOrTurnText, renderer, NULL, NULL, 0.0, NULL, SDL_FLIP_NONE);
-    Texture_render(&currentPredictionText, renderer, NULL, NULL, 0.0, NULL, SDL_FLIP_NONE);
+    // Texture_render(&currentPredictionText, renderer, NULL, NULL, 0.0, NULL, SDL_FLIP_NONE);
     Texture_render(&roundText, renderer, NULL, NULL, 0.0, NULL, SDL_FLIP_NONE);
+    for (int i = 0; i<playerCount; i++){
+      Texture_render(&playerText[i], renderer, NULL, NULL, 0.0, NULL, SDL_FLIP_NONE);
+    }
     break;
   case scoring:
     break;
@@ -177,11 +195,15 @@ static void maingamescene_render(
 static void
 maingamescene_stop() { // --------------------------------------------STOP
   Button_free(&playbutt);
-  for (int i = 0; i < 9; i++) {
+  for (int i = 0; i < round+1; i++) {
     Button_free(&predictionButtons[i]);
   }
+  for (int i = 0; i<playerCount; i++){
+    Texture_free(&playerText[i]);
+  }
+  free(playerText);
   Texture_free(&phaseOrTurnText);
-  Texture_free(&currentPredictionText);
+  // Texture_free(&currentPredictionText);
   Texture_free(&roundText);
   Texture_free(&deck.spriteSheet);
   free(players);
@@ -195,7 +217,7 @@ static void maingamescene_events(
     case deal:
       break;
     case predict:
-  for (int i = 0; i < 9; i++) {
+  for (int i = 0; i < round+1; i++) {
     Button_handleEvent(&predictionButtons[i], e);
   }
       break;
